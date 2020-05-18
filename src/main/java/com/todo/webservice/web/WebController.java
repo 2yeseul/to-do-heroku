3d47bd78819634c8ac9c5ce7e3ebd09af491be00
2yeseul <20152917@sungshin.ac.kr>
2020-05-19 01:13:41 +0900

    [05-19] completed : 대략적인 형태와 데이터 전송 및 조회 / to do: radio 버튼 체크 시, 체크한 td의 id에 해당하는 db update하기 & 밑줄

--- src/main/java/com/todo/webservice/web/WebController.java
+++ src/main/java/com/todo/webservice/web/WebController.java
@@ -1,16 +1,27 @@
 package com.todo.webservice.web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import lombok.AllArgsConstructor;
+import com.todo.webservice.domain.Todo.TODO;
+import com.todo.webservice.domain.Todo.TODORepository;
+import com.todo.webservice.service.PostsService;
 
+import lombok.AllArgsConstructor;
 @Controller
 @AllArgsConstructor
 public class WebController {
-
+		
+	private PostsService postsService;
+	private TODORepository todoRepository;
+	
 	@GetMapping("/main")
-	public String main() {
+	public String main(Model model) {
+		List<TODO> todoList = todoRepository.findAll();
+		model.addAttribute("todoList", todoList);
 		return "main";
 	}
 }

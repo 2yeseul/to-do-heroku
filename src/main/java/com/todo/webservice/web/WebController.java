7cc8b51b97201a0f579d00961f234718f6fb0e61
2yeseul <20152917@sungshin.ac.kr>
2020-05-22 21:42:44 +0900

    [05-22] 카테고리 분류 미완

--- src/main/java/com/todo/webservice/web/WebController.java
+++ src/main/java/com/todo/webservice/web/WebController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.todo.webservice.domain.Todo.TODO;
 import com.todo.webservice.domain.Todo.TODORepository;
+import com.todo.webservice.dto.posts.PostsMainResponseDto;
 import com.todo.webservice.service.PostsService;
 
 import lombok.AllArgsConstructor;
@@ -21,6 +23,30 @@ public class WebController {
 	@GetMapping("/main")
 	public String main(Model model) {
 		List<TODO> todoList = todoRepository.findAll();
+		int size = todoList.size();
+		if(size>0) {
+			Long nextId = todoList.get(size-1).getId()+1;
+			model.addAttribute("nextId", nextId);
+		}
+		model.addAttribute("todoList", todoList);
+		return "main";
+	}
+	
+	@GetMapping("/active")
+	public String active(Model model) {
+		List<PostsMainResponseDto> todoList = postsService.findActive();
+		int size = todoList.size();
+		if(size>0) {
+			Long nextId = todoList.get(size-1).getId()+1;
+			model.addAttribute("nextId", nextId);
+		}
+		model.addAttribute("todoList", todoList);
+		return "main";
+	}
+	
+	@GetMapping("/done")
+	public String done(Model model) {
+		List<PostsMainResponseDto> todoList = postsService.findDone();
 		int size = todoList.size();
 		if(size>0) {
 			Long nextId = todoList.get(size-1).getId()+1;
